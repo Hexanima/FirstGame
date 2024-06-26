c51be95ddfe7c46cf3073aad210b9903ee980b4e
Nicolas Filippelli <nicolasfilippelli22@gmail.com>
2024-06-25 23:58:58 -0300

    Done Player 2.0

--- scripts/player.cs
+++ scripts/player.cs
@@ -6,8 +6,15 @@ public partial class player : CharacterBody2D
 	public const float Speed = 130.0f;
 	public const float JumpVelocity = -300.0f;
 
+	private AnimatedSprite2D animatedSprite2D;
+
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+
+	public override void _Ready()
+	{
+		animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -18,15 +25,42 @@ public partial class player : CharacterBody2D
 			velocity.Y += gravity * (float)delta;
 
 		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		if (Input.IsActionJustPressed("jump") && IsOnFloor())
 			velocity.Y = JumpVelocity;
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
+		// GetAxis es -1, 0, 1
+		float direction = Input.GetAxis("move_left", "move_right");
+
+		// Sprite rotation
+		if (direction > 0)
 		{
-			velocity.X = direction.X * Speed;
+			animatedSprite2D.FlipH = false;
+		}
+		else if (direction < 0)
+		{
+			animatedSprite2D.FlipH = true;
+		}
+
+		// Animations
+		if (IsOnFloor())
+		{
+			if (direction == 0)
+			{
+				animatedSprite2D.Play("idle");
+			}
+			else
+			{
+				animatedSprite2D.Play("run");
+			}
+		}
+		else { 
+			animatedSprite2D.Play("jump");
+		}
+
+		// Movement
+		if (direction != 0)
+		{
+			velocity.X = direction * Speed;
 		}
 		else
 		{
