ce1bedba3958cfba2562c8b8603941d7b7f8e952
Hexanima <nicolasfilippelli22@gmail.com>
2024-06-28 23:34:18 -0300

    Feature: Killzone has damage, player has HP, on 0HP causes Kill

--- scripts/game_manager.cs
+++ scripts/game_manager.cs
@@ -3,27 +3,27 @@ using System;
 
 public partial class game_manager : Node
 {
-    private int score = 0;
-    private Label scoreLabel;
+	private int score = 0;
+	private Label scoreLabel;
 
-    public override void _Ready()
-    {
-        scoreLabel = GetNode<Label>("ScoreLabel");
-        scoreLabel.Text = $"No tienes monedas.";
+	public override void _Ready()
+	{
+		scoreLabel = GetNode<Label>("ScoreLabel");
+		scoreLabel.Text = $"No tienes monedas.";
 
-    }
+	}
 
-    public void AddPoint()
-    {
-        score += 1;
+	public void AddPoint()
+	{
+		score += 1;
 
-        if (score == 1)
-        {
-            scoreLabel.Text = $"Tienes {score} moneda.";
-        }
-        else
-        {
-            scoreLabel.Text = $"Tienes {score} monedas.";
-        }
-    }
+		if (score == 1)
+		{
+			scoreLabel.Text = $"Tienes {score} moneda.";
+		}
+		else
+		{
+			scoreLabel.Text = $"Tienes {score} monedas.";
+		}
+	}
 }
--- scripts/player.cs
+++ scripts/player.cs
@@ -1,78 +1,110 @@
-using Godot;
 using System;
+using Godot;
 
 public partial class player : CharacterBody2D
 {
-	[Export]
-	private float Speed = 130.0f;
+    [Export]
+    private float Speed = 130.0f;
 
-	[Export]
-	private float JumpVelocity = -300.0f;
-	
+    [Export]
+    private float JumpVelocity = -300.0f;
 
-	private AnimatedSprite2D animatedSprite2D;
+    [Export]
+    private int MaxHealth = 100;
+    private int CurrentHealth;
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+    private AnimatedSprite2D animatedSprite2D;
+    private Timer timer;
 
-	public override void _Ready()
-	{
-		animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-	}
+    // Get the gravity from the project settings to be synced with RigidBody nodes.
+    public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
-	public override void _PhysicsProcess(double delta)
-	{
-		Vector2 velocity = Velocity;
+    private void kill()
+    {
+        GD.Print("Dead");
+        Engine.TimeScale = 0.5;
+        this.GetNode<CollisionShape2D>("CollisionShape2D").QueueFree();
+        timer.Start();
+    }
 
-		// Add the gravity.
-		if (!IsOnFloor())
-			velocity.Y += gravity * (float)delta;
+    private void _on_timer_timeout()
+    {
+        // Replace with function body.
+        Engine.TimeScale = 1;
+        this.GetTree().ReloadCurrentScene();
+    }
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("jump") && IsOnFloor())
-			velocity.Y = JumpVelocity;
+    public override void _Ready()
+    {
+        animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+        CurrentHealth = MaxHealth;
+        timer = GetNode<Timer>("Timer");
+        GD.Print(this.CurrentHealth);
+    }
 
-		// GetAxis es -1, 0, 1
-		float direction = Input.GetAxis("move_left", "move_right");
+    public void Hurt(int damage)
+    {
+        this.CurrentHealth -= damage;
 
-		// Sprite rotation
-		if (direction > 0)
-		{
-			animatedSprite2D.FlipH = false;
-		}
-		else if (direction < 0)
-		{
-			animatedSprite2D.FlipH = true;
-		}
+        if (this.CurrentHealth <= 0)
+        {
+            this.kill();
+        }
+    }
 
-		// Animations
-		if (IsOnFloor())
-		{
-			if (direction == 0)
-			{
-				animatedSprite2D.Play("idle");
-			}
-			else
-			{
-				animatedSprite2D.Play("run");
-			}
-		}
-		else
-		{
-			animatedSprite2D.Play("jump");
-		}
+    public override void _PhysicsProcess(double delta)
+    {
+        Vector2 velocity = Velocity;
 
-		// Movement
-		if (direction != 0)
-		{
-			velocity.X = direction * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
+        // Add the gravity.
+        if (!IsOnFloor())
+            velocity.Y += gravity * (float)delta;
 
-		Velocity = velocity;
-		MoveAndSlide();
-	}
+        // Handle Jump.
+        if (Input.IsActionJustPressed("jump") && IsOnFloor())
+            velocity.Y = JumpVelocity;
+
+        // GetAxis es -1, 0, 1
+        float direction = Input.GetAxis("move_left", "move_right");
+
+        // Sprite rotation
+        if (direction > 0)
+        {
+            animatedSprite2D.FlipH = false;
+        }
+        else if (direction < 0)
+        {
+            animatedSprite2D.FlipH = true;
+        }
+
+        // Animations
+        if (IsOnFloor())
+        {
+            if (direction == 0)
+            {
+                animatedSprite2D.Play("idle");
+            }
+            else
+            {
+                animatedSprite2D.Play("run");
+            }
+        }
+        else
+        {
+            animatedSprite2D.Play("jump");
+        }
+
+        // Movement
+        if (direction != 0)
+        {
+            velocity.X = direction * Speed;
+        }
+        else
+        {
+            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+        }
+
+        Velocity = velocity;
+        MoveAndSlide();
+    }
 }
